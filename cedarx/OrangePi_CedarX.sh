7602dda2d5f94f1b8fdabdf00ed0d25f8279be8a
orangepi <buddy.d.zhang@gmail.com>
2017-02-27 19:16:31 +0800

    [PATCH] scripts: fix some hole

--- cedarx/OrangePi_CedarX.sh
+++ cedarx/OrangePi_CedarX.sh
@@ -10,9 +10,6 @@ if [ ! $ROOT = "/home/orangepi/CedarX" ]; then
     echo "$ROOT doesn't exist, Please change dirent into $ROOT"
 fi
 
-# Export dynatic shared library
-export LD_LIBRARY_PATH=/home/orangepi/CedarX/arm-linux-gnueabi:/home/orangepi/CedarX/output/lib:/home/orangepi/CedarX/linuxgnueabi_3.10:/usr/arm-linux-gnueabi/lib/
-
 # Add 32bit dynatic shared library.
 ln -s /usr/arm-linux-gnueabi/lib/ld-linux.so.3 /lib/ld-linux.so.3
 ln -s /home/orangepi/CedarX/arm-linux-gnueabi/libcrypto.so /home/orangepi/CedarX/arm-linux-gnueabi/libcrypto.so.1.0.0
